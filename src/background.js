02512f5f043285fcd045711394c361a887a96e79
spiderPan <panbanglanfeng@gmail.com>
2013-11-24 21:02:38 -0500

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,59 +1,75 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
-    chrome.tabs.sendMessage(info.tabId, {desc: "You got a new song!", url: info.url, format: 'mp3', type: 'music'});
-    console.log('info sent');
-  },
-  // filters
-  {
-    urls: [
-      "http://*/*.mp3*",
-      "https://*/*.mp3*",
-      "http://*.file.xiami.com/h/*", //for xiami vip
-      "http://*.bcbits.com/download/track/*", //for bandcamp.com
-      "https://*.indievox.com/audio-streamer.php*"
-    ],
-    types:[
-      "other", "object"
-    ]
+chrome.webRequest.onBeforeRequest.addListener(function(info) {
+	chrome.tabs.sendMessage(info.tabId, {
+		desc : "You got a new song!",
+		url : info.url,
+		obj:info,
+		format : 'mp3',
+		type : 'music'
+	});
+	console.log('info sent');
+},
+// filters
+{
+	urls : ["http://douban.fm/j/","http://*/*.mp3*", "https://*/*.mp3*", "http://*.file.xiami.com/h/*", //for xiami vip
+	"http://*.bcbits.com/download/track/*", //for bandcamp.com
+	"https://*.indievox.com/audio-streamer.php*"],
+	types : ["other", "object"]
 });
-chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
-    chrome.tabs.sendMessage(info.tabId, {desc: "You got a new song!", url: info.url, format: 'aac', type: 'music'});
-    console.log('info sent');
-  },
-  {
-    urls: [
-      "http://*/*.aac*",
-      "https://*/*.aac*"
-    ],
-    types:[
-      "other", "object"
-    ]
+
+chrome.webRequest.onBeforeRequest.addListener(function(info) {
+	chrome.tabs.sendMessage(info.tabId, {
+		desc : "You got a new ad!",
+		url : info.url,
+		obj:info,
+		format : 'flv',
+		type : 'video'
+	});
+	conso
+	return {cancel: true};
+},
+// filters
+{
+	urls : ["http://*/*.flv*"],
+	types : ["other", "object"]
+},
+["blocking"]
+);
+
+
+
+chrome.webRequest.onBeforeRequest.addListener(function(info) {
+	chrome.tabs.sendMessage(info.tabId, {
+		desc : "You got a new song!",
+		url : info.url,
+		format : 'aac',
+		type : 'music'
+	});
+	console.log('info sent');
+}, {
+	urls : ["http://*/*.aac*", "https://*/*.aac*"],
+	types : ["other", "object"]
 });
-chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
-    chrome.tabs.sendMessage(info.tabId, {desc: "You got a new song!", url: info.url, format: 'm4a', type: 'music'});
-    console.log('info sent');
-  },
-  {
-    urls: [
-      "http://*/*.m4a*",
-      "https://*/*.m4a*"
-    ],
-    types:[
-      "other", "object"
-    ]
+chrome.webRequest.onBeforeRequest.addListener(function(info) {
+	chrome.tabs.sendMessage(info.tabId, {
+		desc : "You got a new song!",
+		url : info.url,
+		format : 'm4a',
+		type : 'music'
+	});
+	console.log('info sent');
+}, {
+	urls : ["http://*/*.m4a*", "https://*/*.m4a*"],
+	types : ["other", "object"]
 });
-chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
-    chrome.tabs.sendMessage(info.tabId, {desc: "You got a new playlist!", url: info.url, type: 'xiami_play_list'});
-    console.log('info sent');
-  },
-  {
-    urls: [
-      "http://www.xiami.com/song/playlist/*" //xiami playlist, to retrieve music info.
-    ],
-    types:[
-      "other", "object"
-    ]
+chrome.webRequest.onBeforeRequest.addListener(function(info) {
+	chrome.tabs.sendMessage(info.tabId, {
+		desc : "You got a new playlist!",
+		url : info.url,
+		type : 'xiami_play_list'
+	});
+	console.log('info sent');
+}, {
+	urls : ["http://www.xiami.com/song/playlist/*" //xiami playlist, to retrieve music info.
+	],
+	types : ["other", "object"]
 });
