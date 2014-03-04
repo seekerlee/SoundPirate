a4b3916fe6acaacdfdc536ab0d0cb1f99e7c8d57
Pan <panbanglanfeng@gmail.com>
2014-03-03 21:29:27 -0500

    Update background.js

--- src/background.js
+++ src/background.js
@@ -22,7 +22,7 @@ chrome.webRequest.onBeforeRequest.addListener(function (info) {
 	},
 // filters
 	{
-		urls  : ["http://douban.fm/*/rda/*.mp3"],
+		urls  : ["http://*/*/rda/*.mp3"],
 		types : ["other", "object"]
 	},
 	["blocking"]
