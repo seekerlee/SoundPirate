2849a05e97c82d50cc2e7d536929ef0bfd158d51
Pan <pan@ubuntu.ubuntu-domain>
2014-03-10 22:23:30 -0400

    Remove All Comments

--- src/background.js
+++ src/background.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-chrome.webRequest.onBeforeRequest.addListener(function (info) {
-		chrome.tabs.sendMessage(info.tabId, {
-			desc   : "You got a new song!",
-			url    : info.url,
-			obj    : info,
-			format : 'mp3',
-			type   : 'music'
-		});
-
-	},
-// filters
-	{
-		urls  : ["http://douban.fm/j/", "http://*/*.mp3*", "https://*/*.mp3*", 
-		"https://songza.com/*.mp4*", "http://songza.com/*.mp4*",//songza.com
-                "https://*.douban.com/*.mp4*", "http://*.douban.com/*.mp4*",
-		"http://*.file.xiami.com/h/*", //for xiami vip
-			"http://*.bcbits.com/download/track/*", //for bandcamp.com
-			"https://*.indievox.com/audio-streamer.php*"],
-		types : ["other", "object"]
-	});
-
-=======
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
     chrome.tabs.sendMessage(info.tabId, {desc: "You got a new song!", url: info.url, format: 'mp3', type: 'music'});
@@ -37,65 +14,22 @@ chrome.webRequest.onBeforeRequest.addListener(
       "http://*.bcbits.com/download/track/*", //for bandcamp.com
       "https://*.indievox.com/audio-streamer.php*",
 	  "https://songza.com/*.mp4*",
-	  "http://songza.com/*.mp4*"//songza.com
+	  "http://songza.com/*.mp4*",//songza.com
+           "https://*.douban.com/*.mp4*",
+	  "http://*.douban.com/*.mp4*"
     ],
     types:[
       "other", "object"
     ]
   }
 );
->>>>>>> origin/patch-1
 
 chrome.webRequest.onBeforeRequest.addListener(function (info) {
 		return {cancel : true};
 	},
 // filters
 	{
-<<<<<<< HEAD
-		urls  : ["http://mr*.douban.com/*/rda/*.mp3"],
-		types : ["other", "object"] 
-	},
-	["blocking"]
-);
-
-chrome.webRequest.onBeforeRequest.addListener(function (info) {
-	chrome.tabs.sendMessage(info.tabId, {
-		desc   : "You got a new song!",
-		url    : info.url,
-		format : 'aac',
-		type   : 'music'
-	});
-	console.log('info sent');
-}, {
-	urls  : ["http://*/*.aac*", "https://*/*.aac*"],
-	types : ["other", "object"]
-});
-chrome.webRequest.onBeforeRequest.addListener(function (info) {
-	chrome.tabs.sendMessage(info.tabId, {
-		desc   : "You got a new song!",
-		url    : info.url,
-		format : 'm4a',
-		type   : 'music'
-	});
-	console.log('info sent');
-}, {
-	urls  : ["http://*/*.m4a*", "https://*/*.m4a*"],
-	types : ["other", "object"]
-});
-chrome.webRequest.onBeforeRequest.addListener(function (info) {
-	chrome.tabs.sendMessage(info.tabId, {
-		desc : "You got a new playlist!",
-		url  : info.url,
-		type : 'xiami_play_list'
-	});
-	console.log('info sent');
-}, {
-	urls  : ["http://www.xiami.com/song/playlist/*" //xiami playlist, to retrieve music info.
-	],
-	types : ["other", "object"]
-});
-=======
-		urls  : ["http://*.douban.com/*/rda/*.mp3"],
+		urls  : ["http://*/*/rda/*.mp3"],
 		types : ["other", "object"]
 	},
 	["blocking"]
@@ -149,4 +83,3 @@ chrome.webRequest.onBeforeRequest.addListener(
       "other", "object"
     ]
 });
->>>>>>> origin/patch-1
