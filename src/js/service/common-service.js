b4a0829fa62aeb902534ca3d26057b4670a5933e
Simon <lishi1608@gmail.com>
2019-07-06 10:24:57 +0800

    exclude

--- src/js/service/common-service.js
+++ src/js/service/common-service.js
@@ -2,10 +2,16 @@ import { addMessageListener, addHeaderListener, sendTabsMsg } from '../browser'
 import { newSound } from '../page/link'
 import { setLatestSoundURL, setLatestSoundName, downloadLatestSound } from '../backgroundAction/soundInfo'
 
+function shouldHandle(url) {
+    // add more exclude here:
+    // exclude gmail https://ssl.gstatic.com/chat/sounds/incoming_message_eb39ce414e3ffba41a8e173581dc7248.mp3
+    return !url.includes('gstatic.com/chat/sounds')
+}
+
 function backAction() {
     addHeaderListener(
         info => {
-            if (info.tabId !== -1) {
+            if (info.tabId !== -1 && shouldHandle(info.url)) {
                 console.log('captured mp3: ' + info.url)
                 info.soundFormat = 'mp3'
                 sendTabsMsg(info.tabId, info)
